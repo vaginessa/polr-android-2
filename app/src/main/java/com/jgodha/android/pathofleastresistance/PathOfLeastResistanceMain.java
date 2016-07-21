577084a151a69b93a06a2c1bb4e57f204d78a8a2
Jitendra Godha <jgodha@JJ-MBP.local>
2016-07-21 08:05:03 -0400

    Moved screenshots to avoid gradle build error. Added support for running data scenarios in the kata.

--- app/src/main/java/com/jgodha/android/pathofleastresistance/PathOfLeastResistanceMain.java
+++ app/src/main/java/com/jgodha/android/pathofleastresistance/PathOfLeastResistanceMain.java
@@ -3,14 +3,30 @@ package com.jgodha.android.pathofleastresistance;
 public class PathOfLeastResistanceMain {
 
     public static  void  main(String[] args) {
-//        String arg = "1 3 5 1 5\;3 8 1 4 9;4 7 1 9 3;9 1 2 8 1;1 3 4 8 2";
-//        String arg = "3 4 1 2 8 6;6 1 8 2 7 4;5 9 3 9 9 5;8 4 1 3 2 6;3 7 2 8 6 4";
-//        String arg = "3 4 1 2 8 6;6 1 8 2 7 4;5 9 3 9 9 5;8 4 1 3 2 6;3 7 2 1 2 3";
-//        String arg = "1 3 5 1;3 8 1 4;4 7 1 9;9 1 2 8";
-//        String arg = "1 3 5;3 8 1;4 7 1";
-//        String arg = "19 10 19 10 19;21 23 20 19 12;20 12 20 11 10";
-        String gridData = args[0];
-        System.out.println(new LeastResistantPathFinder().find(gridData.replaceAll(";", "\n")));
+        LeastResistantPathFinder leastResistantPathFinder = new LeastResistantPathFinder();
+        if(args.length > 0) {
+            String gridData = args[0];
+            System.out.println(leastResistantPathFinder.find(gridData.replaceAll(";", "\n")));
+        } else {
+            //run through the data scenarios mentioned in the kata
+            String[] gridData = new String[] {
+                    "3 4 1 2 8 6\n6 1 8 2 7 4\n5 9 3 9 9 5\n8 4 1 3 2 6\n3 7 2 8 6 4",
+                    "3 4 1 2 8 6\n6 1 8 2 7 4\n5 9 3 9 9 5\n8 4 1 3 2 6\n3 7 2 1 2 3",
+                    "19 10 19 10 19\n21 23 20 19 12\n20 12 20 11 10",
+                    "1 3 5 1 5\n3 8 1 4 9\n4 7 1 9 3\n9 1 2 8 1\n1 3 4 8 2"
+            };
+
+            for(String data : gridData) {
+                printGridData(data);
+                System.out.println(leastResistantPathFinder.find(data));
+                System.out.println("\n");
+            }
+        }
+    }
+
+    private static void printGridData(String gridData) {
+        System.out.println("INPUT");
+        System.out.println(gridData);
     }
 }
 
